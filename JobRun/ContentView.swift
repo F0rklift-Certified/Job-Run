46ca41ea73260914dcf10d7dc5c98fe22af08224
F0rklift-Certified <noah.bondoc11@gmail.com>
2026-04-29 16:43:20 +1000

    Initial Commit

---
+++ JobRun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobRun
+//
+//  Created by NB on 29/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobRun/JobRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobRunApp.swift
+//  JobRun
+//
+//  Created by NB on 29/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct JobRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
